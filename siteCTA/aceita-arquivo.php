e6c736745bad2cf8f48249dc2b9e13e189767465
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-03-25 12:41:12 -0300

    upload_imagem

---
+++ siteCTA/aceita-arquivo.php
@@ -0,0 +1,63 @@
+<?php
+
+$destino = 'img/' . $_FILES['arquivo']['name'];
+ 
+$arquivo_tmp = $_FILES['arquivo']['name'];
+ 
+move_uploaded_file( $arquivo_tmp, $destino  );
+//move_uploaded_file($_files['arquivo']['tmp_name'] , $_files['arquivo']['name'])
+
+?>
+
+<?php
+/******
+ * Upload de imagens
+ ******/
+ 
+// verifica se foi enviado um arquivo
+if ( isset( $_FILES[ 'arquivo' ][ 'name' ] ) && $_FILES[ 'arquivo' ][ 'error' ] == 0 ) {
+    echo 'Você enviou o arquivo: <strong>' . $_FILES[ 'arquivo' ][ 'name' ] . '</strong><br />';
+    echo 'Este arquivo é do tipo: <strong > ' . $_FILES[ 'arquivo' ][ 'type' ] . ' </strong ><br />';
+    echo 'Temporáriamente foi salvo em: <strong>' . $_FILES[ 'arquivo' ][ 'tmp_name' ] . '</strong><br />';
+    echo 'Seu tamanho é: <strong>' . $_FILES[ 'arquivo' ][ 'size' ] . '</strong> Bytes<br /><br />';
+ 
+    $arquivo_tmp = $_FILES[ 'arquivo' ][ 'tmp_name' ];
+    $nome = $_FILES[ 'arquivo' ][ 'name' ];
+     
+        // Pega a extensao
+    $extensao = pathinfo ( $nome, PATHINFO_EXTENSION );
+ 
+    // Converte a extensao para mimusculo
+    $extensao = strtolower ( $extensao );
+ 
+    // Somente imagens, .jpg;.jpeg;.gif;.png
+    // Aqui eu enfilero as extesões permitidas e separo por ';'
+    // Isso server apenas para eu poder pesquisar dentro desta String
+    if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
+        // Cria um nome único para esta imagem
+        // Evita que duplique as imagens no servidor.
+        // Evita nomes com acentos, espaços e caracteres não alfanuméricos
+        $novoNome = uniqid ( time () ) . $extensao;
+ 
+        // Concatena a pasta com o nome
+        $destino = 'img / ' . $novoNome;
+ 
+        // tenta mover o arquivo para o destino
+        if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
+            echo 'Arquivo salvo com sucesso em : <strong>' . $destino . '</strong><br />';
+//            echo '< img src = "' . $destino . '" />';
+        }
+        else
+            echo 'Erro ao salvar o arquivo. Aparentemente você não tem permissão de escrita.<br />';
+    }
+    else
+        echo 'Você poderá enviar apenas arquivos "*.jpg;*.jpeg;*.gif;*.png"<br />';
+}
+else
+    echo 'Você não enviou nenhum arquivo!';
+
+
+
+//$imagem = 'images/' . $id_noticia . '.jpg';
+//if( file_exists( $imagem ) )
+//    echo '<img src="' . $imagem . '" />
---
+++ siteCTA/upload-arquivo.php
@@ -0,0 +1,7 @@
+<form method="post" enctype="multipart/form-data" action="aceita-arquivo.php">
+   Selecione uma imagem: <input name="arquivo" type="file" />
+   <br />
+   <input type="submit" value="Salvar" />
+</form>
+
+
