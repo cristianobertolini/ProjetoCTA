6407006cc30f5af06a2e4f1eb25cfda7d1d9dc61
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-04-09 19:39:42 -0300

    Base de Dados Atualizada
    Inserção da tabela categoria_usuario, e usuario_categoria_usuario.
    Inserção do campo cidade e estado na tabela usuário

--- bancoDados/colabad.sql
+++ bancoDados/colabad.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 28-Mar-2016 às 15:43
+-- Generation Time: 10-Abr-2016 às 00:38
 -- Versão do servidor: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -30,7 +30,40 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `cat_codigo` int(11) NOT NULL AUTO_INCREMENT,
   `cat_nome` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`cat_codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`cat_codigo`, `cat_nome`) VALUES
+(1, 'Pessoas Publicas'),
+(2, 'Bandeiras'),
+(3, 'Lugares'),
+(4, 'Institucionais'),
+(5, 'Futebol'),
+(6, 'Outros');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria_usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria_usuario` (
+  `cat_usu_codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `cat_usu_nome` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cat_usu_codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Extraindo dados da tabela `categoria_usuario`
+--
+
+INSERT INTO `categoria_usuario` (`cat_usu_codigo`, `cat_usu_nome`) VALUES
+(1, 'usuario'),
+(2, 'audiodescritor'),
+(3, 'revisor');
 
 -- --------------------------------------------------------
 
@@ -51,7 +84,14 @@ CREATE TABLE IF NOT EXISTS `imagens` (
   PRIMARY KEY (`img_codigo`),
   KEY `cat_codigo` (`cat_codigo`),
   KEY `usu_codigo` (`usu_codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `imagens`
+--
+
+INSERT INTO `imagens` (`img_codigo`, `usu_codigo`, `img_data`, `img_hora`, `cat_codigo`, `img_audiodescricao`, `img_nome`, `img_nome_original`, `img_extensao`) VALUES
+(2, 23, '2016-03-28', '11:19:00', 2, 'A bandeira do Brasil é formada por um retângulo verde, no qual está inserido bem no meio um grande um losango amarelo, cujo centro possui um círculo azul com vinte e sete estrelas brancas e uma faixa branca, que divide o círculo ao meio. Na faixa branca contém a frase: “Ordem e Progresso”. \r\nNota proêmia: Cada elemento da bandeira possui um significado, sendo o verde o símbolo da robustez das matas brasileiras; O amarelo representa as riquezas minerais do solo; A cor azul ilustra o céu; E o branco, a paz; Cada uma das 27 estrelas brancas representa um estado braspngileiro e o Distrito Federal;\r\n', 'Bandeira', 'Bandeira do Brasil.png', 'png');
 
 -- --------------------------------------------------------
 
@@ -64,17 +104,42 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `usu_senha` varchar(150) DEFAULT NULL,
   `usu_nome` varchar(150) DEFAULT NULL,
   `usu_email` varchar(150) DEFAULT NULL,
-  `usu_escolaridade` varchar(150) DEFAULT NULL,
-  `usu_endereco` varchar(150) DEFAULT NULL,
+  `usu_escolaridade` int(11) DEFAULT NULL,
+  `usu_estado` int(11) DEFAULT NULL,
+  `usu_cidade` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`usu_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`usu_codigo`, `usu_senha`, `usu_nome`, `usu_email`, `usu_escolaridade`, `usu_endereco`) VALUES
-(22, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', '', '');
+INSERT INTO `usuario` (`usu_codigo`, `usu_senha`, `usu_nome`, `usu_email`, `usu_escolaridade`, `usu_estado`, `usu_cidade`) VALUES
+(23, '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Juliana de Fatima da Silva', 'julianafatsil@hotmail.com', 0, 0, ''),
+(24, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
+(25, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
+(26, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
+(27, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario_categoria_usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario_categoria_usuario` (
+  `usu_codigo` int(11) NOT NULL,
+  `cat_usu_codigo` int(11) NOT NULL,
+  PRIMARY KEY (`usu_codigo`,`cat_usu_codigo`),
+  KEY `cat_usu_codigo` (`cat_usu_codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario_categoria_usuario`
+--
+
+INSERT INTO `usuario_categoria_usuario` (`usu_codigo`, `cat_usu_codigo`) VALUES
+(23, 1);
 
 --
 -- Constraints for dumped tables
@@ -86,6 +151,13 @@ INSERT INTO `usuario` (`usu_codigo`, `usu_senha`, `usu_nome`, `usu_email`, `usu_
 ALTER TABLE `imagens`
   ADD CONSTRAINT `imagens_ibfk_1` FOREIGN KEY (`cat_codigo`) REFERENCES `categoria` (`cat_codigo`),
   ADD CONSTRAINT `imagens_ibfk_2` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`);
+
+--
+-- Limitadores para a tabela `usuario_categoria_usuario`
+--
+ALTER TABLE `usuario_categoria_usuario`
+  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_2` FOREIGN KEY (`cat_usu_codigo`) REFERENCES `categoria_usuario` (`cat_usu_codigo`),
+  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
