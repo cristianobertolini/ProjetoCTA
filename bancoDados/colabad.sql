f1ae2363161337f9d0a2d1bca5c373187c5dd945
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-04-11 22:34:17 -0300

    Melhorias siteCTA
    Melhorias no Banco de dados e Arquivo Equipe

--- bancoDados/colabad.sql
+++ bancoDados/colabad.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Abr-2016 às 00:38
+-- Generation Time: 12-Abr-2016 às 03:31
 -- Versão do servidor: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `categoria_usuario` (
   `cat_usu_codigo` int(11) NOT NULL AUTO_INCREMENT,
   `cat_usu_nome` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`cat_usu_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `categoria_usuario`
@@ -81,6 +81,7 @@ CREATE TABLE IF NOT EXISTS `imagens` (
   `img_nome` varchar(50) DEFAULT NULL,
   `img_nome_original` varchar(150) DEFAULT NULL,
   `img_extensao` varchar(5) DEFAULT NULL,
+  `img_situacao` int(11) NOT NULL,
   PRIMARY KEY (`img_codigo`),
   KEY `cat_codigo` (`cat_codigo`),
   KEY `usu_codigo` (`usu_codigo`)
@@ -90,8 +91,8 @@ CREATE TABLE IF NOT EXISTS `imagens` (
 -- Extraindo dados da tabela `imagens`
 --
 
-INSERT INTO `imagens` (`img_codigo`, `usu_codigo`, `img_data`, `img_hora`, `cat_codigo`, `img_audiodescricao`, `img_nome`, `img_nome_original`, `img_extensao`) VALUES
-(2, 23, '2016-03-28', '11:19:00', 2, 'A bandeira do Brasil é formada por um retângulo verde, no qual está inserido bem no meio um grande um losango amarelo, cujo centro possui um círculo azul com vinte e sete estrelas brancas e uma faixa branca, que divide o círculo ao meio. Na faixa branca contém a frase: “Ordem e Progresso”. \r\nNota proêmia: Cada elemento da bandeira possui um significado, sendo o verde o símbolo da robustez das matas brasileiras; O amarelo representa as riquezas minerais do solo; A cor azul ilustra o céu; E o branco, a paz; Cada uma das 27 estrelas brancas representa um estado braspngileiro e o Distrito Federal;\r\n', 'Bandeira', 'Bandeira do Brasil.png', 'png');
+INSERT INTO `imagens` (`img_codigo`, `usu_codigo`, `img_data`, `img_hora`, `cat_codigo`, `img_audiodescricao`, `img_nome`, `img_nome_original`, `img_extensao`, `img_situacao`) VALUES
+(2, 23, '2016-03-28', '11:19:00', 2, 'A bandeira do Brasil é formada por um retângulo verde, no qual está inserido bem no meio um grande um losango amarelo, cujo centro possui um círculo azul com vinte e sete estrelas brancas e uma faixa branca, que divide o círculo ao meio. Na faixa branca contém a frase: “Ordem e Progresso”. \r\nNota proêmia: Cada elemento da bandeira possui um significado, sendo o verde o símbolo da robustez das matas brasileiras; O amarelo representa as riquezas minerais do solo; A cor azul ilustra o céu; E o branco, a paz; Cada uma das 27 estrelas brancas representa um estado braspngileiro e o Distrito Federal;\r\n', 'Bandeira', 'Bandeira do Brasil.png', 'png', 0);
 
 -- --------------------------------------------------------
 
@@ -101,25 +102,24 @@ INSERT INTO `imagens` (`img_codigo`, `usu_codigo`, `img_data`, `img_hora`, `cat_
 
 CREATE TABLE IF NOT EXISTS `usuario` (
   `usu_codigo` int(100) NOT NULL AUTO_INCREMENT,
-  `usu_senha` varchar(150) DEFAULT NULL,
   `usu_nome` varchar(150) DEFAULT NULL,
   `usu_email` varchar(150) DEFAULT NULL,
+  `usu_senha` varchar(150) DEFAULT NULL,
   `usu_escolaridade` int(11) DEFAULT NULL,
   `usu_estado` int(11) DEFAULT NULL,
   `usu_cidade` varchar(100) DEFAULT NULL,
+  `usu_descricao` text NOT NULL,
   PRIMARY KEY (`usu_codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`usu_codigo`, `usu_senha`, `usu_nome`, `usu_email`, `usu_escolaridade`, `usu_estado`, `usu_cidade`) VALUES
-(23, '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Juliana de Fatima da Silva', 'julianafatsil@hotmail.com', 0, 0, ''),
-(24, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
-(25, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
-(26, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, ''),
-(27, 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', 0, 0, '');
+INSERT INTO `usuario` (`usu_codigo`, `usu_nome`, `usu_email`, `usu_senha`, `usu_escolaridade`, `usu_estado`, `usu_cidade`, `usu_descricao`) VALUES
+(23, 'Juliana de Fatima da Silva', 'julianafatsil@hotmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 2, 3, 'Frederico Wetphalen', 'Pouco conhecimento em audio_descricao'),
+(28, 'exemplo', 'julianafatsil@hotmail.com', 'ju', 2, 2, 'Frederico Westphalen', 'Pouco Conhecimento em audidescricao'),
+(30, 'teste', 'julianafatsil@hotmail.com', '2e6f9b0d5885b6010f9167787445617f553a735f', 0, 0, 'Seberi', 'oioi');
 
 -- --------------------------------------------------------
 
@@ -156,8 +156,8 @@ ALTER TABLE `imagens`
 -- Limitadores para a tabela `usuario_categoria_usuario`
 --
 ALTER TABLE `usuario_categoria_usuario`
-  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_2` FOREIGN KEY (`cat_usu_codigo`) REFERENCES `categoria_usuario` (`cat_usu_codigo`),
-  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`);
+  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`),
+  ADD CONSTRAINT `usuario_categoria_usuario_ibfk_2` FOREIGN KEY (`cat_usu_codigo`) REFERENCES `categoria_usuario` (`cat_usu_codigo`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- siteCTA/QSequipe.php
+++ siteCTA/QSequipe.php
@@ -18,6 +18,56 @@
         <div class="w3-container w3-section w3-padding-large w3-card-4 w3-light-grey">
             <h1>Equipe</h1>
         </div>
+        
+          <ul class="w3-ul w3-card-4">
+            <li class="w3-padding-hor-16">
+                <span onclick="this.parentElement.style.display='none'" 
+                class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+                <img src="img/img_avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                <span class="w3-xlarge">Juliana de Fatima da Silva </span><br>
+                <span> E-mail:julianafatsil@hotmail.com</span>
+              </li> 
+              
+            <li class="w3-padding-hor-16">
+            <span onclick="this.parentElement.style.display='none'" 
+            class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+            <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+            <span class="w3-xlarge">Talliny Dalla Nora</span><br>
+            <span>E-mail:tallinydn@gmail.com</span>
+          </li>
+          
+           <li class="w3-padding-hor-16">
+                <span onclick="this.parentElement.style.display='none'" 
+                class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+                <img src="img/img_avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                <span class="w3-xlarge">Victória Lieberknecht</span><br>
+                <span>E-mail:vic_lieber@hotmail.com</span>
+              </li> 
+                  
+              <li class="w3-padding-hor-16">
+                <span onclick="this.parentElement.style.display='none'" 
+                class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+                  <img src="img/img_avatar2.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                <span class="w3-xlarge">Prof. Cristiano Bertolini</span><br>
+                <span>E-mail:cristiano.bertolini@ufsm.br</span>
+              </li>
+
+              <li class="w3-padding-hor-16">
+                <span onclick="this.parentElement.style.display='none'" 
+                class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+                <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                <span class="w3-xlarge">Profa. Adriana Pereira</span><br>
+                <span>E-mail:adriana.pereira@ufsm.br</span>
+              </li> 
+               <li class="w3-padding-hor-16">
+                <span onclick="this.parentElement.style.display='none'" 
+                class="w3-closebtn w3-padding w3-margin-right w3-medium"></span>
+                <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:60px">
+                <span class="w3-xlarge">Profa. Janaína Gomes</span><br>
+                <span>E-mail:jgomes.fw@gmail.com</span>
+              </li> 
+            </ul>
+
 
         <?php
             include("rodape.php");
