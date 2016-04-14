5a83e837c9a4604d5e8db1de796fb6733b19e5a4
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-04-13 23:33:34 -0300

    Melhorias siteCTA

--- siteCTA/QSdescricao.php
+++ siteCTA/QSdescricao.php
@@ -17,6 +17,10 @@
     <div id="main" class="w3-container" style="margin-left:300px;margin-top:118px">    
         <div class="w3-container w3-section w3-padding-large w3-card-4 w3-light-grey">
             <h1>O colabAD</h1>
+            <p>O ColabAD: consiste em uma plataforma colaborativa que possibilita o
+               acesso à áudio-descrição de imagens para pessoas com deficiência
+               visual e profissionais que precisam do recurso de áudio-descrição em
+               suas imagens.</p>
         </div>
 
         <?php
--- siteCTA/cadastro_usuario.php
+++ siteCTA/cadastro_usuario.php
@@ -1,68 +1,87 @@
-<?php include("include/config.php"); 
-      include("include/conexao.php");
-      include("include/funcoes.php");
+ <div class="w3-container">
+    <div class="w3-center"><br>
+             <h1>Cadastro de Usuário</h1>
+    </div>
+      <form id="cadastro_usuario" action="cadastro_usuario.php" method="POST">         
+      <div class="w3-section">
+        <label class="w3-label"><strong>Nome</strong></label>
+        <input class="w3-input w3-border w3-margin-bottom" id="nome" name="nome" type="text" placeholder="Insira seu nome completo" required>
+        
+        <label class="w3-label"><strong>Login</strong></label>
+        <input class="w3-input w3-border" id="email" name="email" type="text" placeholder="Insira seu email" required>
+        
+         <label class="w3-label"><strong>Senha</strong></label>
+        <input class="w3-input w3-border w3-margin-bottom" id="senha" name="senha" type="password" placeholder="Insira sua senha" required>
+        
+        <label class="w3-label" for="escolaridade"><strong>Escolaridade</strong>
+            <select name="escolaridade" id="escolaridade" required>
+                                <option value=""> --- selecione ---</option>
+                                <option value="Primeiro Grau Incompleto">1&deg; Grau - Prim&aacute;rio Incompleto</option>
+                                <option value="Primeiro Grau Completo">1&deg; Grau - Prim&aacute;rio Completo</option>
+                                <option value="Ginasial Incompleto">1&deg; Grau - Ginasial Incompleto</option>
+                                <option value="Ginasial Completo">1&deg; Grau - Ginasial Completo</option>
+                                <option value="Segundo Grau Incompleto">2&deg; Grau - Colegial Incompleto</option>
+                                <option value="Segundo Grau Completo">2&deg; Grau - Colegial Completo</option>
+                                <option value="Terceiro Grau Incompleto">3&deg; Grau - Superior Incompleto</option>
+                                <option value="Terceiro Grau Completo">3&deg; Grau - Superior Completo</option>
+                                <option value="Especializa&ccedil;&atilde;o">Especializa&ccedil;&atilde;o</option>
+                                <option value="Mestrado">Mestrado</option>
+                                <option value="Doutorado">Doutorado</option>
+                </select>  <br />
+        </label>    
+         
+        <label class="w3-label" for="estado"><strong>Estado</strong>
+                            <select  name="estado" id="estado"required>
+                                <option value="">---Selecione---</option>
+                                <option value="AC">Acre</option>
+                                <option value="AL">Alagoas</option>
+                                <option value="AP">Amapá</option>
+                                <option value="AM">Amazonas</option>
+                                <option value="BA">Bahia</option>
+                                <option value="CE">Ceará</option>
+                                <option value="DF">Distrito Federal</option>
+                                <option value="ES">Espirito Santo</option>
+                                <option value="GO">Goiás</option>
+                                <option value="MA">Maranhão</option>
+                                <option value="MS">Mato Grosso do Sul</option>
+                                <option value="MT">Mato Grosso</option>
+                                <option value="MG">Minas Gerais</option>
+                                <option value="PA">Pará</option>
+                                <option value="PB">Paraíba</option>
+                                <option value="PR">Paraná</option>
+                                <option value="PE">Pernambuco</option>
+                                <option value="PI">Piauí</option>
+                                <option value="RJ">Rio de Janeiro</option>
+                                <option value="RN">Rio Grande do Norte</option>
+                                <option value="RS">Rio Grande do Sul</option>
+                                <option value="RO">Rondônia</option>
+                                <option value="RR">Roraima</option>
+                                <option value="SC">Santa Catarina</option>
+                                <option value="SP">São Paulo</option>
+                                <option value="SE">Sergipe</option>
+                                <option value="TO">Tocantins</option>
+                            </select>
+        </label> <br> 
+         <label class="w3-label"><strong>Cidade</strong></label>
+        <input class="w3-input w3-border w3-margin-bottom"  id="cidade" name="cidade" type="text" placeholder="Insira o nome de sua cidade" required>
+        </div>
+          <label class="w3-label"><strong>Conte-nos qual é o seu conhecimento sobre audiodescrição:</strong></label> 
+          <textarea class="w3-input w3-border" name="descricao" id="descricao"required></textarea></br>
+                <label class="w3-label"required><strong>Marque seu tipo de usuário</strong> (Para ser aceito seu usuário será avaliado pelo administrador)<br>
 
+                    <br><input id='categoria' name='categoria[]' type='checkbox' value='1'>
+                    <label for='categoria1'><strong>Usuário</strong>(O usuário tem permissão upload de imagens que deseja audiodescrição)</label>
+                    <br><input id='categoria' name='categoria[]' type='checkbox' value='2'>
+                    <label for='categoria2'><strong>Audiodescritor</strong>(O usuário tem permissão de fazer audio descricao nas imagens)</label>
+                    <br><input id='categoria' name='categoria[]' type='checkbox' value='3'>
+                    <label for='categoria3'><strong>Revisor</strong>(O usuário tem permissão de fazer revisões das imagens com audiodescrição)</label>
+
+      </div>
       
-    $login = $mysqli->real_escape_string($_POST['email']);
-    $senha = $mysqli->real_escape_string($_POST['senha']);
-    $nome = $mysqli->real_escape_string($_POST['nome']);
-    $escolaridade = $mysqli->real_escape_string($_POST['escolaridade']);
-    $estado = $mysqli->real_escape_string($_POST['estado']); 
-    $cidade = $mysqli->real_escape_string($_POST['cidade']); 
-    $descricao = $mysqli->real_escape_string($_POST['descricao']); 
-    
-     foreach ($_POST['categoria'] as $key => $value){
-                $categoria[$key] = $value;
-        }    
-        $categoria = $mysqli->fetch_array($_POST['categoria']);
-        echo $categoria;
-
-        if (($codigo >= 0)  && ($codigo != '')) {
-            $sql = "DELETE FROM `usuario_categoria` WHERE `usu_codigo`= '$codigo'";     
-            $mysqli->query($sql); 
-
-            $sql = "UPDATE `usuario`
-                    SET `usu_login`='$login',`usu_senha`='".SHA1($senha)."',`usu_nome`='$nome',`usu_email`='$email'
-                        `usu_matricula`='$matricula',`usu_situacao`='$situacao'
-                    WHERE `usu_codigo`= '$codigo'";
-        } else {
-            $sql = "INSERT INTO `usuario` (`usu_nome`, `usu_email`, `usu_senha`, `usu_escolaridade`, `usu_estado`, `usu_cidade`, `usu_descricao`) 
-                    VALUES ('$nome', '$login','".SHA1($senha)."', '$escolaridade', '$estado', '$cidade', '$descricao');";        
-            }    
-
-        $mysqli->query($sql);
-        $queryInsert->execute();   
-        if (($codigo >= 0)  && ($codigo != '')) {
-            $sqlUsu = "SELECT `usu_codigo`
-                     FROM `usuario`  
-                     WHERE `usu_codigo`= '$codigo'    
-                     LIMIT 1";
-        } else {
-            $sqlUsu = "SELECT `usu_codigo`
-                     FROM `usuario`  
-                     WHERE (`usu_email` = '". $login ."') AND (`usu_senha` = '". SHA1($senha) ."')    
-                     LIMIT 1";     
-        }
-        $queryUsu = $mysqli->query($sqlUsu);    
-
-        if (mysqli_num_rows($queryUsu) > 0) { 
-            if (!empty($categoria)) {
-                $N = count($categoria);
-                $resultado = $queryUsu->fetch_assoc();
-                for($i=0; $i < $N; $i++)
-                {
-                    $sqlCategoria = "INSERT INTO `usuario_categoria_usuario` (`usu_codigo`, `cat_usu_codigo`) VALUES ('". $resultado['usu_codigo'] ."', '". $categoria[$i] ."')";
-                    $mysqli->query($sqlCategoria);
-                }
-            }
-        }  
-
-        include("../include/funcoes.php");
-
-        $emailmsg = montaMensagem($login, $senha); 
-
-        echo "<script>location.href='cadastrar.php?mensagem=success&texto=Usuário inserido com sucesso! $emailret';</script>";
-        $mysqli->Close();
-        die();
-    ?>
-
+    <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+      <button class="w3-btn w3-btn-block w3-green">Enviar</button>
+      
+    </div>
+</form>
+  </div>
+      </div>
