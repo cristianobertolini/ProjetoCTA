ea187a55af63e7b62703173a0aa9cbdb57d862b1
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-04-30 18:30:02 -0300

    Melhorias em Quem Somos

--- siteCTA/QSdescricao.php
+++ siteCTA/QSdescricao.php
@@ -17,10 +17,22 @@
     <div id="main" class="w3-container" style="margin-left:300px;margin-top:118px">    
         <div class="w3-container w3-section w3-padding-large w3-card-4 w3-light-grey">
             <h1>O colabAD</h1>
-            <p>O ColabAD: consiste em uma plataforma colaborativa que possibilita o
-               acesso à áudio-descrição de imagens para pessoas com deficiência
-               visual e profissionais que precisam do recurso de áudio-descrição em
-               suas imagens.</p>
+            <p style="text-align: justify;">O sistema ColabAD consiste em uma plataforma colaborativa que possibilita o acesso à 
+                AD de imagens para pessoas com deficiência visual e profissionais que precisam do 
+                recurso de AD em suas imagens. Nossa missão é missão formar uma rede de áudio-descritores 
+                colaboradores que possam atuar de forma livre mas comprometida em prover AD de qualidade. 
+            </p>
+            
+              <p style="text-align: justify;">O sistema é dividido por perfis de usuários: (i) usuário comum que gera demanda de imagens; 
+                  (ii) usuário especializado para realizar AD em imagens e/ou trabalhar na revisão de imagens.
+            </p>
+            
+             <p style="text-align: justify;">Qualquer usuário pode cadastrar imagens que deseja AD e elas ficam disponíveis para os 
+                 áudio-descritores e possam ser revisadas, pelo menos uma vez, para que se torne pública a 
+                 qualquer pessoa mediante autorização de veiculação de imagem. Desta forma, as imagens são 
+                 categorizadas e ficam disponíveis publicamente para que outras pessoas possam utilizar. 
+                 O ColabAD é um sistema aberto, desenvolvido para facilitar e disseminar a AD.
+            </p>
             <br/>
             <br/>
             <br/>
