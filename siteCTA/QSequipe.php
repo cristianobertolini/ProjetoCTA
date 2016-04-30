4642f77256d60696955e969c6d10ccfbba6e1607
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-04-30 17:57:33 -0300

    Melhorias na Equipe

--- siteCTA/QSequipe.php
+++ siteCTA/QSequipe.php
@@ -21,33 +21,38 @@
           <ul class="w3-ul w3-card-4">
             <li class="w3-padding-hor-16">
                 <img src="img/img_avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar feminino">
-                <span class="w3-medium">Juliana de Fatima da Silva </span><br>
+                <span class="w3-medium"><strong>Juliana de Fatima da Silva</strong></span><br>
                 <span> E-mail:julianafatsil@hotmail.com</span>
               </li> 
             <li class="w3-padding-hor-16">
             <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar feminino">
-            <span class="w3-medium">Talliny Dalla Nora</span><br>
+            <span class="w3-medium"><strong>Talliny Dalla Nora</strong></span><br>
             <span>E-mail:tallinydn@gmail.com</span>
           </li>
            <li class="w3-padding-hor-16">
                 <img src="img/img_avatar6.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar feminino">
-                <span class="w3-medium">Victória Lieberknecht</span><br>
+                <span class="w3-medium"><strong>Victória Lieberknecht</strong></span><br>
                 <span>E-mail:vic_lieber@hotmail.com</span>
               </li> 
               <li class="w3-padding-hor-16">
                   <img src="img/img_avatar_2.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar masculino">
-                <span class="w3-medium">Prof. Cristiano Bertolini</span><br>
+                <span class="w3-medium"><strong>Prof. Cristiano Bertolini</strong></span><br>
                 <span>E-mail:cristiano.bertolini@ufsm.br</span>
               </li>
               <li class="w3-padding-hor-16">
                 <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar feminino">
-                <span class="w3-medium">Profa. Adriana Pereira</span><br>
+                <span class="w3-medium"><strong>Profa. Adriana Pereira</strong></span><br>
                 <span>E-mail:adriana.pereira@ufsm.br</span>
               </li> 
                <li class="w3-padding-hor-16">
                 <img src="img/img_avatar5.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="avatar feminino">
-                <span class="w3-medium">Profa. Janaína Gomes</span><br>
+                <span class="w3-medium"><strong>Profa. Janaína Gomes</strong></span><br>
                 <span>E-mail:jgomes.fw@gmail.com</span>
+              </li> 
+               <li class="w3-padding-hor-16">
+                    <img src="img/logo_acessibilidade.png" class="w3-left w3-circle w3-margin-right" style="width:30px" alt="logo do grupo de acessibilidade de Santa Maria">
+                    <span class="w3-medium">O Sistema ColabAD é desenvolvido em conjunto com o <strong>Núcleo de Acessibilidade UFSM </strong>- <a href="http://w3.ufsm.br/acessibilidade."> http://w3.ufsm.br/acessibilidade/</a></span><br>
+                </br>
               </li> 
             </ul>
 
