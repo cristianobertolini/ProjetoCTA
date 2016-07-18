8c29803328878cab862addf60c6e42ee84d6d0f0
Juliana da Silva <julianafatsil@mail.ufsm.br>
2016-07-17 22:56:11 -0300

    Atualizações em na página Descrição

--- siteCTA/QSdescricao.php
+++ siteCTA/QSdescricao.php
@@ -5,44 +5,38 @@
             include("cabecalho.php");
             $pagina = 'QS';
         ?>    
-            
-        <title>colabAD | O colabAD </title>           
+        <title>colabAD | Descrição </title>           
     </head>    
-
     <body>
-        
-    <?php
-        include("navegacao.php");
-    ?>
+        <?php
+            include("navegacao.php");
+        ?>
     <div id="main" class="w3-container" style="margin-left:300px;margin-top:118px">    
         <div class="w3-container w3-section w3-padding-large w3-card-4 w3-light-grey">
             <h1>O colabAD</h1>
+                      
         </div>
-         <div class="w3-container">
-             <br>
-            <p style="text-align: justify;">O sistema ColabAD consiste em uma plataforma colaborativa que possibilita o acesso à 
-                AD de imagens para pessoas com deficiência visual e profissionais que precisam do 
-                recurso de AD em suas imagens. Nossa missão é missão formar uma rede de áudio-descritores 
-                colaboradores que possam atuar de forma livre mas comprometida em prover AD de qualidade. 
+                   
+        <div class="w3-container w3-section w3-pale-green w3-leftbar w3-rightbar w3-border-green">
+         <br>
+            <p style="text-align: justify;"> O sistema ColabAD consiste em uma plataforma colaborativa que possibilita o acesso à 
+            áudio-descrição de imagens para pessoas com deficiência visual e profissionais que precisam do 
+            recurso de áudio-descrição em suas imagens. Nossa missão é missão formar uma rede de áudio-descritores 
+            colaboradores que possam atuar de forma livre mas comprometida em prover áudio-descrição de qualidade. 
             </p>
-            
-              <p style="text-align: justify;">O sistema é dividido por perfis de usuários: (i) usuário comum que gera demanda de imagens; 
-                  (ii) usuário especializado para realizar AD em imagens e/ou trabalhar na revisão de imagens.
+             <p style="text-align: justify;">O sistema é dividido por perfis de usuários: (1) usuário comum que gera demanda de audio-decrição de imagens; 
+              (2) usuário especializado para realizar áudio-descrição em imagens; (3) usuário especializado para realizar revisão de imagens e torná-las públicas.Tanto o 
+              áudio-descritor como o revisor podem fazer o cancelamento de imagens que considerarem inadequadas para publicação.
             </p>
-            
-             <p style="text-align: justify;">Qualquer usuário pode cadastrar imagens que deseja AD e elas ficam disponíveis para os 
-                 áudio-descritores e possam ser revisadas, pelo menos uma vez, para que se torne pública a 
-                 qualquer pessoa mediante autorização de veiculação de imagem. Desta forma, as imagens são 
-                 categorizadas e ficam disponíveis publicamente para que outras pessoas possam utilizar. 
-                 O ColabAD é um sistema aberto, desenvolvido para facilitar e disseminar a AD.
+            <p style="text-align: justify;">Qualquer usuário pode cadastrar imagens que deseja áudio-descrição e elas ficam disponíveis para os 
+             áudio-descritores e podem ser revisadas pelo menos uma vez, para que se torne pública a 
+             qualquer pessoa mediante autorização de veiculação de imagem. Desta forma, as imagens são 
+             categorizadas e ficam disponíveis publicamente para que outras pessoas possam utilizar. 
+             O ColabAD é um sistema aberto, desenvolvido para facilitar e disseminar a áudio-descrição.
             </p>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
         </div>
+    </div>
+
 
         <?php
             include("rodape.php");
